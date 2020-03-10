3cc3700342012e4e42be7a9491eb309f5df1392f
huykks1999 <lequanghuygg@gmail.com>
2020-03-10 21:23:30 +0700

    nộp bài

---
+++ java/Assignment_4/Car.java
@@ -0,0 +1,15 @@
+package Assignment_4;
+
+public class Car {
+int speed;
+double regularPrice;
+String color;
+
+public Car(int speed, int regularPrice, String color) {
+	// TODO Auto-generated constructor stub
+}
+
+double getSalesPrice() {
+	return getSalesPrice();
+};
+}
