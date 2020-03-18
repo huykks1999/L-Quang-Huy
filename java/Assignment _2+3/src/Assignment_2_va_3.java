35b906f6bd2276f5cf3e502d84e50faf494d1734
huykks1999 <lequanghuygg@gmail.com>
2020-03-18 13:12:07 +0700

    java

--- java/Assignment _2+3/src/Assignment_2_va_3.java
+++ java/Assignment _2+3/src/Assignment_2_va_3.java
@@ -1,11 +1,12 @@
-package Assignments;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.NoSuchElementException;
+import java.util.Random;
 
 public class Assignment_2_va_3 {
 
 	public static void main(String[] args) {
-//Questions 1:
+		// Questions 1:
 
 		String họ[] = { "Lê", "Trần", "Nguyễn", "Cao", "Trương", "Đào", "Tống" };
 		String Tên_đệm[] = { "Văn", "Phong", "Hồng", "Thị", "Quang", "Thanh", "Tiến" };
@@ -27,15 +28,15 @@ public class Assignment_2_va_3 {
 		 * ,"Trương Thị Hoa"};
 		 */
 
-//Questions 2:
+		// Questions 2:
 		String ID[] = { "SV1234567", "SV3344334", "SV7644543", "SV1275358", "SV0787654", "SV1276345", "SV6543454",
 				"SV3745764", "SV6784123", "SV1847273" };
 
-//Questions 3:
+		// Questions 3:
 		String Birth_date[] = { "29/10/1991", "29/09/1991", "29/08/1991", "29/07/1991", "29/06/1991", "29/05/1991",
 				"29/04/1991", "29/03/1991", "29/02/1991", "29/01/1991" };
 
-//Questions 4:
+		// Questions 4:
 		System.out.println("Full_name                      ID                     Date      ");
 		System.out.println("------------------------------------------------------------------");
 		for (int i = 0; i < ID.length; i++) {
@@ -43,7 +44,7 @@ public class Assignment_2_va_3 {
 			System.out.println(Full_name[i] + "              " + ID[i] + "                " + Birth_date[i]);
 		}
 
-//assignment 3:
+		// assignment 3:
 
 		// Questions 1:
 		int diem_1[] = { 2, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
@@ -64,32 +65,34 @@ public class Assignment_2_va_3 {
 		for (int i = 0; i < avg_diem.length; i++) {
 			l1.add(avg_diem[i]);
 		}
-		System.out.println(Collections.max(l1));
-		System.out.println(l1.size());
+
 
 		System.out.println(
-				"_____________________________________________________________________________________________");
+				"\n\n_____________________________________________________________________________________________");
 		System.out.println(
 				"Full_name                ID               Date         đ1     đ2    đ3    đ4    đ5       AVG");
 		System.out.println(
 				"---------------------------------------------------------------------------------------------");
+		try {
+			do {
+				for (int i = 0; i < ID.length; i++) {
+					double g = (double) Collections.max(l1);
 
-		do {
-			for (int i = 0; i < ID.length; i++) {
-				double g = (double) Collections.max(l1);
+					if (avg_diem[i] == g) {
+						System.out.println(Full_name[i] + "         " + ID[i] + "        " + Birth_date[i] + "      "
+								+ diem_1[i] + "       " + diem_2[i] + "     " + diem_3[i] + "     " + diem_4[i]
+								+ "     " + diem_5[i] + "                    " + avg_diem[i]);
+					} else {
+						continue;
+					}
+					l1.remove(g);
 
-				if (avg_diem[i] == g) {
-					System.out.println(Full_name[i] + "         " + ID[i] + "        " + Birth_date[i] + "      "
-							+ diem_1[i] + "       " + diem_2[i] + "     " + diem_3[i] + "     " + diem_4[i] + "     "
-							+ diem_5[i] + "                    " + avg_diem[i]);
-				} else {
-					continue;
 				}
-				l1.remove(g);
+			} while (l1.size() > 0);
+		} catch (NoSuchElementException e) {
 
-			}
-		} while (l1.size() > 0);
+		}
 
-		
 	}
+
 }
